06e48c5d046fedd0d99c2f256711b7e15560ed8f
hyejinxx <haejinjung1110@gmail.com>
2023-03-04 01:13:24 +0900

    feat: 상담사 페이지 riverpod 적용

--- lib/screen/counselor/counselor_screen.dart
+++ lib/screen/counselor/counselor_screen.dart
@@ -5,40 +5,54 @@ import 'package:diviction_user/screen/counselor/requested_counselor_screen.dart'
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../config/style.dart';
 
-class CounselorScreen extends StatefulWidget {
-  const CounselorScreen({super.key});
+final pageProvider = StateProvider<int>((ref) => 0);
 
-  @override
-  State<CounselorScreen> createState() => _CounselorScreenState();
-}
+class CounselorScreen extends ConsumerWidget {
+  CounselorScreen({super.key});
 
-@override
-class _CounselorScreenState extends State<CounselorScreen> {
   final PageController _pageController = PageController(initialPage: 0);
-  int _selectedIndex = 0;
 
   @override
-  void initState() {
-    super.initState();
+  Widget build(BuildContext context, WidgetRef ref) {
     _pageController.addListener(() {
-      setState(() {
-        _selectedIndex = _pageController.page!.round();
-      });
+      ref.read(pageProvider.notifier).state = _pageController.page!.round();
     });
-  }
-
-  @override
-  void dispose() {
-    _pageController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    int index = ref.watch(pageProvider);
     final page = [FindCounselorScreen(), RequestedCounselorScreen()];
+
+    Widget bottomBar() {
+      return InkWell(
+          onTap: () {
+            _pageController.animateToPage(index == 1 ? 0 : 1,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.easeInOut);
+          },
+          child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                border: Border.symmetric(
+                    horizontal: BorderSide(
+                        width: 1, color: Palette.bottomBoxBorderColor)),
+              ),
+              padding: const EdgeInsets.all(12),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(index == 0 ? '내가 한 요청 보기' : 'counselor list',
+                      style: TextStyles.bottomTextStyle),
+                  Icon(index == 0
+                      ? Icons.arrow_right_sharp
+                      : Icons.arrow_left_sharp)
+                ],
+              )));
+    }
+
     return Stack(
       children: [
         PageView.builder(
@@ -53,34 +67,5 @@ class _CounselorScreenState extends State<CounselorScreen> {
         )
       ],
     );
-  }
-
-  Widget bottomBar() {
-    return InkWell(
-        onTap: () {
-          _pageController.animateToPage(_selectedIndex == 1 ? 0 : 1,
-              duration: const Duration(milliseconds: 500),
-              curve: Curves.easeInOut);
-        },
-        child: Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              border: Border.symmetric(
-                  horizontal: BorderSide(
-                      width: 1, color: Palette.bottomBoxBorderColor)),
-            ),
-            padding: const EdgeInsets.all(12),
-            width: MediaQuery.of(context).size.width,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text(_selectedIndex == 0 ? '내가 한 요청 보기' : '상담사 찾기',
-                    style: TextStyles.bottomTextStyle),
-                Icon(_selectedIndex == 0
-                    ? Icons.arrow_right_sharp
-                    : Icons.arrow_left_sharp)
-              ],
-            )));
   }
 }
