af7c6ed7d25bc1b09643672a69eba9aa0a7606fe
rlaxodud214 <rlaxodud214@naver.com>
2023-03-09 02:38:09 +0900

    design: CircularSeekBar UI 수정

--- lib/screen/survey/survey_result.dart
+++ lib/screen/survey/survey_result.dart
@@ -13,13 +13,15 @@ class _SurveyResultState extends State<SurveyResult> {
   @override
   Widget build(BuildContext context) {
     int _progress = 30;
-    Map<String, Color> ColorMap = {
-      '안전': Color(0xFF50C878),
-      '경고': Color(0xFFFDB813),
-      '주의': Color(0xFFFF8C00),
-      '위험': Color(0xFFEE2C2C),
-      '심각': Color(0x00B22222),
-    };
+
+    List<Color> Linear_colors = const [
+      Colors.blue,
+      Color(0xFF50C878),
+      Color(0xFFFDB813),
+      Color(0xFFFF8C00),
+      Color(0xFFEE2C2C),
+      Color(0xFFB22222),
+    ];
 
     return Scaffold(
       appBar: const MyAppbar(
@@ -40,46 +42,45 @@ class _SurveyResultState extends State<SurveyResult> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Stack(children: [
-                    CircularSeekBar(
-                      width: double.infinity,
-                      height: 250,
-                      progress: _progress.toDouble(),
-                      maxProgress: 40,
-                      barWidth: 10,
-                      startAngle: 45,
-                      sweepAngle: 270,
-                      dashWidth: 5,
-                      dashGap: 1.5,
-                      strokeCap: StrokeCap.butt,
-                      progressGradientColors: const [
-                        Colors.blue,
-                        Color(0xFF50C878),
-                        Color(0xFFFDB813),
-                        Color(0xFFFF8C00),
-                        Color(0xFFEE2C2C),
-                        Color(0xFFB22222),
-                      ],
-                      innerThumbRadius: 5,
-                      innerThumbStrokeWidth: 3,
-                      innerThumbColor: Colors.white,
-                      outerThumbRadius: 5,
-                      outerThumbStrokeWidth: 10,
-                      outerThumbColor: Colors.orange,
-                      animation: true,
-                    ),
-                    Positioned(
-                      top: MediaQuery.of(context).size.height * 0.1,
-                      left: MediaQuery.of(context).size.width * 0.43 - 30,
-                      child: Text(
-                        '$_progress점',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 30,
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.25,
+                    child: Stack(
+                      children: [
+                        CircularSeekBar(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height * 0.25,
+                          progress: _progress.toDouble(),
+                          maxProgress: 40,
+                          barWidth: 8,
+                          startAngle: 45,
+                          sweepAngle: 270,
+                          dashWidth: 5,
+                          dashGap: 1.5,
+                          strokeCap: StrokeCap.butt,
+                          trackColor: Colors.black12,
+                          progressGradientColors: Linear_colors,
+                          innerThumbRadius: 5,
+                          innerThumbStrokeWidth: 3,
+                          innerThumbColor: Colors.white,
+                          outerThumbRadius: 5,
+                          outerThumbStrokeWidth: 10,
+                          outerThumbColor:
+                              getColorForValue(_progress, 40, Linear_colors),
+                          animation: true,
                         ),
-                      ),
+                        // 아래 Text를 CircularSeekBar의 정중앙에 놓는 방법 구상중
+                        Text(
+                          '$_progress점',
+                          maxLines: 1,
+                          softWrap: false,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ],
                     ),
-                  ]),
+                  ),
                   _SizedBox(0.03),
                   Text(
                     "3단계. 유해",
@@ -116,5 +117,24 @@ class _SurveyResultState extends State<SurveyResult> {
     return SizedBox(
       height: MediaQuery.of(context).size.height * value,
     );
+  }
+
+  Color getColorForValue(int value, int maxValue, List<Color> colors) {
+    // 각 구간 별로 값을 분리
+    final numSegments = colors.length - 1;
+    final segmentValue = maxValue / numSegments;
+
+    // 현재 value가 어떤 구간에 속하는지 계산
+    final segmentIndex =
+        (value / segmentValue).floor().clamp(0, numSegments - 1);
+
+    // 해당 구간의 시작 색상, 끝 색상, 그리고 구간 내에서의 비율 계산
+    final startColor = colors[segmentIndex];
+    final endColor = colors[segmentIndex + 1];
+    final segmentProgress =
+        (value - (segmentIndex * segmentValue)) / segmentValue;
+
+    // 시작 색상과 끝 색상, 비율을 이용하여 현재 value에 해당하는 색상 계산
+    return Color.lerp(startColor, endColor, segmentProgress)!;
   }
 }
