831f4a4f40a25db412e43ea6d0912d32d5f2e11c
hyejinxx <haejinjung1110@gmail.com>
2023-03-11 00:53:29 +0900

    fix: detenv 오류 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:diviction_user/screen/splash_screen.dart';
 import 'package:diviction_user/screen/bottom_nav.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stack_trace/stack_trace.dart' as stack_trace;
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "lib/config/.env");
   runApp(ProviderScope(child: MyApp()));
 }
 
