3be2bf448661ff7b8f22a2d33aa409b90fa13a1e
hyejinxx <haejinjung1110@gmail.com>
2023-03-22 23:29:59 +0900

    design: 상담사 페이지 디자인 변경

--- lib/screen/counselor/counselor_screen.dart
+++ lib/screen/counselor/counselor_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:diviction_user/screen/counselor/find_counselor_screen.dart';
-import 'package:diviction_user/screen/counselor/requested_counselor_screen.dart';
+import 'package:diviction_user/screen/chat/chatlist_screen.dart';
 import 'package:diviction_user/service/chat_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,7 +22,7 @@ class CounselorScreen extends ConsumerWidget {
     });
     int index = ref.watch(pageProvider);
     final page = [
-      const RequestedCounselorScreen(),
+      const ChatListScreen(),
       const FindCounselorScreen(),
     ];
 
@@ -43,31 +43,34 @@ class CounselorScreen extends ConsumerWidget {
                     offset: Offset(0, 0),
                   )
                 ],
-                color: Colors.blue,
+                color: Palette.appColor,
                 borderRadius: BorderRadius.all(Radius.circular(10)),
               ),
               margin: const EdgeInsets.all(15),
-              padding: const EdgeInsets.all(15),
+              padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 15),
               width: MediaQuery.of(context).size.width - 30,
               alignment: Alignment.centerLeft,
-              height: 100,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   if (index == 0) ...[
-                    const Text.rich(
-                      TextSpan(
-                          text: 'Want find Counselor?\n',
-                          style: TextStyles.counselorTitle,
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: ' find your counselor',
-                                style: TextStyles.counselorMiddle)
-                          ]),
-                      textAlign: TextAlign.start,
+                    const Text(
+                      'Want find Counselor?',
+                      style: TextStyles.counselorTitle,
                     ),
+                    // Text.rich(
+                    //   TextSpan(
+                    //       text: 'Want find Counselor?\n',
+                    //       style: TextStyles.counselorTitle,
+                    //       children: <TextSpan>[
+                    //         TextSpan(
+                    //             text: ' find your counselor',
+                    //             style: TextStyles.counselorMiddle)
+                    //       ]),
+                    //   textAlign: TextAlign.start,
+                    // ),
                     const Icon(
                       Icons.arrow_right_sharp,
                       size: 50,
@@ -93,27 +96,85 @@ class CounselorScreen extends ConsumerWidget {
               )));
     }
 
-    return Stack(
-      children: [
-        PageView.builder(
-            itemCount: page.length,
-            findChildIndexCallback: (key) {
-              //important here:
-              if (index > 0) {
-                return index;
-              } else {
-                return null;
-              }
-            },
-            controller: _pageController,
-            itemBuilder: (context, index) {
-              return page[index];
-            }),
-        Positioned(
-          bottom: 0,
-          child: bottomBar(),
-        )
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Palette.appColor,
+          title: const Text('Divicion',
+              style: TextStyle(color: Colors.white, fontSize: 23)),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Stack(children: [
+          Container(
+              color: Palette.appColor,
+              padding: const EdgeInsets.only(top: 30),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Row(
+                    //   mainAxisSize: MainAxisSize.max,
+                    //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //   children: [
+                    //     Flexible(
+                    //         flex: 1,
+                    //         child: Container(
+                    //             padding: EdgeInsets.all(10),
+                    //             margin: const EdgeInsets.symmetric(
+                    //                 horizontal: 10, vertical: 4),
+                    //             decoration: BoxDecoration(
+                    //                 borderRadius: BorderRadius.circular(10),
+                    //                 color: index == 0
+                    //                     ? Colors.white
+                    //                     : Colors.blue),
+                    //             child: const Text(
+                    //               'Counselor',
+                    //               style: TextStyles.answerTextStyle,
+                    //             ))),
+                    //     Flexible(
+                    //       flex: 1,
+                    //       child: Container(
+                    //           padding: EdgeInsets.all(10),
+                    //           margin: const EdgeInsets.symmetric(
+                    //               horizontal: 10, vertical: 4),
+                    //           decoration: BoxDecoration(
+                    //               borderRadius: BorderRadius.circular(10),
+                    //               color:
+                    //                   index == 0 ? Colors.white : Colors.blue),
+                    //           child: const Text(
+                    //             'Counselor',
+                    //             style: TextStyles.answerTextStyle,
+                    //           )),
+                    //     )
+                    //   ],
+                    // ),
+                    Expanded(
+                        child: Container(
+                      decoration: const BoxDecoration(
+                        color: Palette.appColor2,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(30)),
+                      ),
+                      child: PageView.builder(
+                          itemCount: page.length,
+                          findChildIndexCallback: (key) {
+                            //important here:
+                            if (index > 0) {
+                              return index;
+                            } else {
+                              return null;
+                            }
+                          },
+                          controller: _pageController,
+                          itemBuilder: (context, index) {
+                            return page[index];
+                          }),
+                    ))
+                  ])),
+          Positioned(
+            bottom: 0,
+            child: bottomBar(),
+          )
+        ]));
   }
 }
